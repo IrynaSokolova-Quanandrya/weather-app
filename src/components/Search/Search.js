d31aaca3598d5495a5ae049cf75da7f0b1180650
Iryna <irasokolova25@ukr.net>
2022-01-31 19:32:16 +0200

    stylize header

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,16 +1,16 @@
 import s from './Search.module.css'
 
+
 export default function Search(){
     return(
         <form  className={s.SearchForm}>
-                    
                     <input
                         className={s.SearchForm__input}
                         name='query'
                         type="text"
                         autoComplete="off"
                         autoFocus
-                        placeholder="Search images and photos"
+                        placeholder="Enter your city"
                     />
                     <button type="submit" className={s.SearchForm__button}>
                         <span className={s.SearchForm__buttonLabel}>Search</span>
--- src/components/pages/Cities/CitiesPage.js
+++ src/components/pages/Cities/CitiesPage.js
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom'
+import CityCard from '../../CityCard/CityCard'
 import Search from '../../Search/Search'
 
 export default function CitiesPage(){
     return(
         <>
             <Search/>
-            <h2>Cities page</h2>
+            <CityCard/>
             <Link to={'/city/:cityId'}>See more</Link>
             <Link to={'/city/:cityId/hourly'}>Hourly forecast</Link>
         </>
